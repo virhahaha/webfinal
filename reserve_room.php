418ece95a8505f5c47a12dcfbfc40d455fccc4af
zhaohuazhen <zhaohuazhen160@gmail.com>
2016-04-19 15:27:00 -0400

    reserve_room

---
+++ reserve_room.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <h2>Reservation deatil:</h2>
+    <input type="hidden" name="room_id" value="<?php echo $_GET['room_id']; ?>" />
+    
+    <label>Contact: </label>
+    <label>Check In Date: </label>
+    <label>Check Out Date: </label>
+    <label>Reservation Date: </label>
+    		<button type="submit" value="Reserve">Reserve</button>
+			<a href="index.html" ><button type="reset">Reset</button></a>
+</html>
+    
