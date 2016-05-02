a115354f8cdbe528af47d2385ec37a36fe9f9ea2
zhaohuazhen <zhaohuazhen160@gmail.com>
2016-05-01 23:54:41 -0400

    register

---
+++ register.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	
+<body>
+     
+		<div class="container">
+			<div class="panel-body">
+				<div class="row">
+					<strong>Register</strong>
+				</div>
+				<br />
+				<div class="row">
+					// call register controller
+				
+							<label class="control-label" for="name">Name: </label>
+							
+								<input class="form-control" name="name" id="name"
+									placeholder="full name" type="text">
+						
+							<label class="control-label" for="username">User name: </label>
+							
+								<input class="form-control" name="username" id="username"
+									placeholder="username" type="text">
+						
+
+							<label class="control-label" for="password">Password: </label>
+						
+								<input class="form-control" name="password" id="password"
+									placeholder="Password" type="password">
+							
+					
+
+							<label class="control-label" for="email">Email: </label>
+						
+								<input class="form-control" name="email" id="email"
+									placeholder="email" type="email">
+						
+
+							<label class="control-label" for="address">Address: </label>
+							
+								<input class="form-control" name="address" id="address"
+									placeholder="address" type="text">
+						
+
+							<label class="control-label" for="contact">Contact: </label>
+							
+								<input class="form-control" name="contact" id="contact"
+									placeholder="contact" type="text">
+							
+						
+
+
+						
+							<button type="submit" class="btn" value="resigter">Resister</button>
+							<a href="index.html" target="_blank"><button
+									class="btn" data-dismiss="modal" type="reset">Reset</button></a>
+						
+			</div>
+		</div>
+	</div>
+	     
+	</body>
+
+</html>
